eb91a74e3edb9bc2aed3c27d927efb459699bc5f
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-03 14:54:28 -0400

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿var total = 0;
+
+for (var index = 1; index <= 50; index++)
+    {
+    if (index % 5 == 0 || index % 7 == 0)
+        {
+        total = total + index;
+        }
+
+
+
+    }
+    Console.WriteLine($"Total is {total}");
